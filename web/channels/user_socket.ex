d1e0db73d63f26a3cc754284191218cbd09fdc91
Teapane <tyler.stephen.long@gmail.com>
2016-02-03 09:21:31 -0700

    Adds form submission and channel code

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ExampleApp.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", ExampleApp.RoomChannel
+   channel "rooms:*", ExampleApp.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -9,6 +9,24 @@ import socket from "./socket"
 
 class App {
   static init() {
+    var username = $("#username")
+    var msgBody = $("#message")
+
+    let socket = new Socket("/socket")
+    socket.connect()
+    socket.onClose(event => console.log("Close all the connections!"))
+
+    var channel = socket.channel("rooms:lobby", {})
+    channel.join()
+      .receive("error", () => console.log("Connection couldn't be established"))
+
+    msgBody.off("keypress")
+      .on("keypress", event => {
+        if (event.keyCode == 13) {
+          console.log('[${username.val()}] ${msgBody.val()}}')
+          msgBody.val("")
+        }
+      })
     console.log("Is this thing on?")
   }
 }
